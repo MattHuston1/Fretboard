8061be15aba6cb8664a85f992bf229bb1600b056
Matt Huston <matthuston1@aol.com>
2020-04-19 18:43:48 -0600

    clicking note displays below and can delete

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const root = document.documentElement
 
 const fretboard = document.querySelector('.fretboard')
+const tabs = document.querySelector('.tabs')
 const selectedInstrumentSelector = document.querySelector('#instrument-selector')
 const accidentalSelector = document.querySelector('.accidental-selector')
 const numOfFretsSelector = document.querySelector('#number-of-frets')
 const showAllNotesSelector = document.querySelector('#show-all-notes')
+const selectedNotes = document.querySelector('.selectedNotes')
+const deleteButton = document.querySelector('.deleteButton')
+const deleteAllButton = document.querySelector('.deleteAllButton')
+
 
 let numOfFrets = 20
 const singleFretMarkPositions = [3, 5, 7, 9, 15, 17, 19, 21]
@@ -29,6 +34,7 @@ const app = {
     this.setupFretboard()
     this.setupEventListeners()
     this.setupSelectedInstrumentSelector()
+    this.setupTabs()
   },
   setupFretboard() {
     fretboard.innerHTML = ''
@@ -36,7 +42,9 @@ const app = {
     //add strings to fretboard
     for (let i = 0; i < numOfStrings; i++) {
       let string = tools.createElement('div')
+      // string.id = i ++
       string.classList.add('string')
+      string.setAttribute('id', i)
       fretboard.appendChild(string)
 
       //create frets
@@ -61,6 +69,21 @@ const app = {
       }
     }
   },
+  setupTabs(){
+    tabs.innerHTML = ''
+    for (let i = 0; i < numOfStrings; i++) {
+      let string = tools.createElement('div')
+      string.classList.add('tabstring')
+      string.setAttribute('id', 'tabString' + i)
+      tabs.appendChild(string)
+      for (let j = 0; j < 50; j++) {
+        const note = tools.createElement('div')
+        note.classList.add('tabNote')
+        note.setAttribute('id', 'tabNote' + j)
+        tabs.appendChild(note)
+      }
+    }
+  },
   generateNoteNames(noteIndex, accidentals) {
     noteIndex = noteIndex % 12;
     let noteName;
@@ -69,35 +92,76 @@ const app = {
     } else if (accidentals === 'sharps') {
       noteName = notesSharp[noteIndex];
     }
-    return noteName; 
+    return noteName;
   },
   setupSelectedInstrumentSelector() {
-    for(instrument in instrumentTuningPresets){
+    for (instrument in instrumentTuningPresets) {
       let instrumentOption = tools.createElement('option', instrument)
       selectedInstrumentSelector.appendChild(instrumentOption)
     }
   },
   showNoteDot(event) {
-    if (event.target.classList.contains('noteFret')) {
+    if (event.target.classList.add.contains('noteFret')) {
       event.target.style.setProperty('--note-dot-opacity', 1)
     }
   },
   hideNoteDot(event) {
-    event.target.style.setProperty('--note-dot-opacity', 0)
+    // console.log(event.target.onclick)
+    // if (fretboard.clicked == false) {
+      event.target.style.setProperty('--note-dot-opacity', 0)
+      
+    // }
   },
   setupEventListeners() {
     fretboard.addEventListener('mouseover', this.showNoteDot)
     fretboard.addEventListener('mouseout', this.hideNoteDot)
+    fretboard.addEventListener('click', (event) => {
+      // console.log(event.target.offsetParent.id)
+      // console.log(event.target.dataset.note)
+      let noteSelected = tools.createElement('h1')
+      noteSelected.textContent = event.target.dataset.note
+      noteSelected.setAttribute('id', event.target.offsetParent.id)
+      noteSelected.classList.add.add('noteSelected')
+      selectedNotes.appendChild(noteSelected)
+      // console.log(selectedNotes)
+      // console.log(selectedNotes.children)
+      // for (let i = 0; i < selectedNotes.children.length; i++) {
+      //   console.log(selectedNotes.children[i].id + 'this')
+      //   if (selectedNotes.children[i].id !== event.target.offsetParent.id) {
+      //     console.log('oops')
+      //   }else{
+          
+      //     console.log('nope')
+      //   }
+      // }
+      // console.log(selectedNotes)
+      // this.showNoteDot
+    })
+    deleteButton.addEventListener('click', (event) => {
+      // selectedNotes.removeChild(selectedNotes.lastChild)
+      if (selectedNotes.hasChildNodes()) {
+        selectedNotes.removeChild(selectedNotes.lastChild)
+      } else {
+        console.log('What are you trying to delete?')
+      }
+    })
+    deleteAllButton.addEventListener('click', (event) => {
+      if (selectedNotes.hasChildNodes()) {
+        selectedNotes.textContent = ''
+      } else {
+        console.log('What are you trying to delete?')
+      }
+    })
     selectedInstrumentSelector.addEventListener('change', (event) => {
-      selectedInstrument = event.target.value 
+      selectedInstrument = event.target.value
       numOfStrings = instrumentTuningPresets[selectedInstrument].length
       this.setupFretboard()
     })
     accidentalSelector.addEventListener('click', (event) => {
-      if(event.target.classList.contains('acc-select')) {
+      if (event.target.classList.contains('acc-select')) {
         accidentals = event.target.value
         this.setupFretboard()
-      }else{
+      } else {
         return;
       }
     })
@@ -118,6 +182,7 @@ const app = {
         this.setupFretboard()
       }
     })
+
   }
 }
 
